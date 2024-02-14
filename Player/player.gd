53eef7370c72c3d50432973d19f466b2230a2838
John David Lao <jdavidlao@gmail.com>
2024-02-14 20:33:35 +0800

    update

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@onready var animations = $AnimationPlayer
 
+#Gets keyboard Input
 func handleInput():
 	var moveDirection = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	velocity = moveDirection*speed
+
+#Handles walk animation
+func updateAnimation():
+	if velocity.length() == 0:
+		if animations.is_playing():
+			animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animations.play("walk" + direction)
 	
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
